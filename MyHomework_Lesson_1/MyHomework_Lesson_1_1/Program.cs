acc8b9d4f6d9d9ae80cc7619148ccba0a4b4db26
Toxichys <ToxichSpam@bk.ru>
2022-01-30 23:08:44 +0200

    Finished
    Finished

---
+++ MyHomework_Lesson_1/MyHomework_Lesson_1_1/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+
+namespace MyHomework_Lesson_1_1
+{
+    class Program
+    {
+        public class TestTaskOne
+        {
+            public long number { get; set; }
+            public bool Expected { get; set; }
+        }
+
+        static void TestCheckingPrimeNumber(TestTaskOne testTaskOne)
+        {
+            CheckingPrimeNumber checkingPrimeNumber = new CheckingPrimeNumber();
+            bool result = checkingPrimeNumber.CheckingPrimeNumberMethod(testTaskOne.number);
+            if (result == testTaskOne.Expected)
+            {
+                Console.WriteLine("VALID TEST");
+            }
+            else
+            {
+                Console.WriteLine("INVALID TEST");
+            }
+        }
+ 
+        static void Main(string[] args)
+        {
+            TestTaskOne testTaskOne1 = new TestTaskOne()
+            {
+                number = 6,
+                Expected = false,
+            };
+
+            TestTaskOne testTaskOne2 = new TestTaskOne()
+            {
+                number = 7,
+                Expected = true,
+            };
+
+            TestCheckingPrimeNumber(testTaskOne1);
+            TestCheckingPrimeNumber(testTaskOne2);
+        }
+    }
+}
